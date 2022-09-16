ebf5d45b5a2812ec01aab44f5e963b62314106e1
gin041420 <roginarolloque@gmail.com>
2022-09-16 16:05:50 +0800

    Added a Multiplication code

--- activity1.java
+++ activity1.java
@@ -11,4 +11,11 @@ public class activity1{
     public static double subtract(double num1, double num2){
         return num1 - num2;
     }
+
+    // Multiplication
+   
+    public static double multiply(double num1, double num2){
+        return num1 * num2;
+    }
+
 }
