c809c3c19f97004ec6d2887ec4ff985bb7f37f6a
wenay <21103866@usc.edu.ph>
2022-09-16 21:26:01 +0800

    added a file fruitcolor

---
+++ activity2/activitytwo/fruitcolor.java
@@ -0,0 +1,13 @@
+    public class fruitcolor extends fruitsMain{
+    
+    String fruitcolor;
+    
+
+    public String getColor(){
+        return fruitcolor;        
+    }
+
+    public void setColor(String newColor){
+        this.fruitcolor = newColor;    
+    }    
+}
