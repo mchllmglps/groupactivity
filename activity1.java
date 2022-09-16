3e29a11c4c3fb8d1dd1a72c0951ff42637727762
wenay <21103866@usc.edu.ph>
2022-09-16 17:03:11 +0800

    Merge branch 'feature/display' into develop

--- activity1.java
+++ activity1.java
@@ -1,11 +1,5 @@
 public class activity1{
    
-    // Addition
-   
-    public static double add(double num1, double num2){
-        return num1 + num2;
-    }
-
     // Subtraction
    
     public static double subtract(double num1, double num2){
