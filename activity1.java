7ced69a45a0f0daaea07fed8862adab6e68a4d71
mchllmglps <103492271+wenaypial@users.noreply.github.com>
2022-09-16 15:32:08 +0800

    Update activity1.java
    Added subtraction class code

--- activity1.java
+++ activity1.java
@@ -1,6 +1,14 @@
 public class activity1{
+   
+    // Addition
+   
     public static double add(double num1, double num2){
         return num1 + num2;
     }
 
+    // Subtraction
+    
+    public static double sub(double num1, double num2){
+        return num1 - num2;
+    }
 }
