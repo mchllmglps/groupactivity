e14e5b9375ad6fc1941e349d8d1f6bed9edf13a2
mchllmglps <mchllmglps@gmail.com>
2022-09-16 15:38:05 +0800

    addition

--- activity1.java
+++ activity1.java
@@ -6,9 +6,4 @@ public class activity1{
         return num1 + num2;
     }
 
-    // Subtraction
-    
-    public static double sub(double num1, double num2){
-        return num1 - num2;
-    }
 }
