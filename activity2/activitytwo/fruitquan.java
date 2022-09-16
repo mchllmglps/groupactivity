a068e593c6fc846de6dfe910890f40b3118ee3bb
wenay <21103866@usc.edu.ph>
2022-09-16 21:12:28 +0800

    added a file fruitquan

---
+++ activity2/activitytwo/fruitquan.java
@@ -0,0 +1,15 @@
+package activitytwo;
+
+public class fruitquan extends fruitsMain{
+    String fruitquantity;
+    
+
+    public String getQuan(){
+        return fruitquantity;        
+    }
+
+    public void setName(String newQuantity){
+        this.fruitquantity = newQuantity;    
+    }    
+    
+}
