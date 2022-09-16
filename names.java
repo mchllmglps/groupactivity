fd6427d94974a147ad4f13af3370ac32bdd8d40b
gin041420 <roginarolloque@gmail.com>
2022-09-16 15:13:11 +0800

    Update names.java
    Added a name

--- names.java
+++ names.java
@@ -2,5 +2,6 @@ public class names {
     public static void main(String[] args) {
         System.out.println("Michelle Maglapus");
         System.out.println("Rowena Pial");
+        System.out.println("Rogina Rolloque");
     }
 }
