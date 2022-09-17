8d61b63bbdd19b4364716eb68c9055d96554919c
gin041420 <roginarolloque@gmail.com>
2022-09-17 09:41:04 +0800

    Added a file fruitname

---
+++ activity2/activitytwo/fruitname.java
@@ -0,0 +1,14 @@
+package activitytwo;
+
+public class fruitname extends fruitsMain{
+    
+    String fruitname;
+
+    public String getName(){
+        return fruitname;        
+    }
+
+    public void setName(String newName){
+        this.fruitname = newName;    
+    }    
+}
